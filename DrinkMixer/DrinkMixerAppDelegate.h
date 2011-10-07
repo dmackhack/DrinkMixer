cd6bd9754131a9aaf9ca38930343799017dfba4e
David Mackenzie <dmackenzie@David-Mackenzies-Mac.local>
2011-10-07 21:47:25 +1100

    Initial Commit

---
+++ DrinkMixer/DrinkMixerAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DrinkMixerAppDelegate.h
+//  DrinkMixer
+//
+//  Created by David Mackenzie on 7/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrinkMixerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ DrinkMixer/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  DrinkMixer
+//
+//  Created by David Mackenzie on 7/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
